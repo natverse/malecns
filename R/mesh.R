c86267c992e76818dd9ce6e43406c647941b94f6
Gregory Jefferis <jefferis@gmail.com>
2021-10-31 11:12:39 +0000

    Support for passing df to read_mcns_meshes

--- R/mesh.R
+++ R/mesh.R
@@ -4,6 +4,9 @@
 #'
 #' @param type Currently only "small" meshes are supported
 #' @inheritParams malevnc::manc_dvid_annotations
+#' @param df A data.frame containing information about the neurons for which
+#'   meshes will be fetched. Optional and if \code{ids} is a data.frame, then
+#'   that will be used.
 #' @param ... Additional arguments passed to \code{httr::GET}
 #'
 #' @return A \code{\link{neuronlist}} containing one or more \code{mesh3d}
@@ -17,14 +20,23 @@
 #' # or if there's just one mesh, you can get more control
 #' wire3d(ml[[1]])
 #' }
-read_mcns_meshes <- function(ids, type='small', node='neutu', ...) {
+read_mcns_meshes <- function(ids, type='small', node='neutu', df=NULL, ...) {
+  if(is.data.frame(ids)) {
+    df=ids
+  }
+  # fix default rownames, this should really happen in nat
+  # see https://github.com/natverse/nat/pull/467
+  if(!is.null(df) && "bodyid" %in% colnames(df)) {
+    if(all(rownames(df)==seq_len(nrow(df))))
+      rownames(df)=df[['bodyid']]
+  }
   ids=mcns_ids(ids, mustWork=T)
 
   node=with_mcns({
     malevnc:::manc_nodespec(node, several.ok = F)
   })
   res=pbapply::pbsapply(ids, read_mcns_mesh, node=node, ..., simplify = F)
-  return(nat::as.neuronlist(res, AddClassToNeurons=F))
+  return(nat::as.neuronlist(res, AddClassToNeurons=F, df=df))
 }
 
 read_mcns_mesh <- function(id, node, ...) {
