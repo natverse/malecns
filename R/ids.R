47097278cb61cb4ab9b220ae03b0995efc8d7034
Gregory Jefferis <jefferis@gmail.com>
2021-10-31 11:12:57 +0000

    add manc_xyz2bodyid

--- R/ids.R
+++ R/ids.R
@@ -1,3 +1,20 @@
 mcns_ids <- function(ids, ..., dataset=getOption("malecns.dataset")) {
   with_mcns(malevnc::manc_ids(ids, ...))
 }
+
+#' Map XYZ locations to bodyids based on the current mcns dataset
+#'
+#' @param xyz	location in raw dataset pixels
+#' @inheritParams malevnc::manc_xyz2bodyid
+#' @return A character vector of body ids (0 is missing somas / missing
+#'   locations)
+#' @export
+#'
+#' @examples
+#' # the APL
+#' \dontrun{
+#' mcns_xyz2bodyid(cbind(24508, 15674, 26116))
+#' }
+mcns_xyz2bodyid <- function(xyz, node = 'neutu', cache=FALSE) {
+  with_mcns(malevnc::manc_xyz2bodyid(xyz, node=node, cache = cache))
+}
