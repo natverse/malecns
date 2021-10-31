c50de9d300ef19fea6f0c556b9232cf90026b708
Gregory Jefferis <jefferis@gmail.com>
2021-10-31 11:16:42 +0000

    basic package docs

---
+++ R/malecns-package.R
@@ -0,0 +1,15 @@
+#' @keywords internal
+#' @section Package Options: \itemize{
+#'
+#'   \item \code{malecns.dataset} This is to used to keep track of the active
+#'   dataset. At present it is only for expert use and it is not recomended that
+#'   you change this manually.
+#'
+#'   }
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
