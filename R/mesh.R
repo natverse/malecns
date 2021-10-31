6018e127d766f1211d778230d16997b6a4ac584f
Gregory Jefferis <jefferis@gmail.com>
2021-10-31 10:36:23 +0000

    Switch to read_mcns_meshes
    * i.e. read one or more meshes

--- R/mesh.R
+++ R/mesh.R
@@ -1,18 +1,33 @@
 #' Read a mesh for the current segmentation
 #'
-#' @param id A body id
+#' @details \code{small} meshes are low-resolution and generated on demand
+#'
+#' @param type Currently only "small" meshes are supported
+#' @inheritParams malevnc::manc_dvid_annotations
 #' @param ... Additional arguments passed to \code{httr::GET}
 #'
-#' @return A \code{mesh3d} object
+#' @return A \code{\link{neuronlist}} containing one or more \code{mesh3d}
+#'   objects.
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#' m=read_mcns_mesh(1796013202)
-#' wire3d(m)
+#' ml=read_mcns_meshes(1796013202)
+#' plot3d(ml)
+#' # or if there's just one mesh, you can get more control
+#' wire3d(ml[[1]])
 #' }
-read_mcns_mesh <- function(id, ...) {
-  id=malevnc::manc_ids(id)
-  u=sprintf("https://ngsupport-bmcp5imp6q-uk.a.run.app/small-mesh?dvid=https://emdata6-erivan.janelia.org&uuid=c2ece&body=%s&decimation=0.5", id)
+read_mcns_meshes <- function(ids, type='small', node='neutu', ...) {
+  ids=mcns_ids(ids, mustWork=T)
+
+  node=with_mcns({
+    malevnc:::manc_nodespec(node, several.ok = F)
+  })
+  res=pbapply::pbsapply(ids, read_mcns_mesh, node=node, ..., simplify = F)
+  return(nat::as.neuronlist(res, AddClassToNeurons=F))
+}
+
+read_mcns_mesh <- function(id, node, ...) {
+  u=sprintf("https://ngsupport-bmcp5imp6q-uk.a.run.app/small-mesh?dvid=https://emdata6-erivan.janelia.org&uuid=%s&body=%s&decimation=0.5", node, id)
   malevnc:::read_neuroglancer_mesh(u, ...)
 }
