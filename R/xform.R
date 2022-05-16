e89375941cd85e1484acf77ae31e29532b49e495
Gregory Jefferis <jefferis@gmail.com>
2022-05-17 00:24:28 +0100

    correct halfbrain limits
    * this means out of bounds warning is no longer triggered by xform

--- R/xform.R
+++ R/xform.R
@@ -7,7 +7,7 @@ mcns_register_xforms <- function() {
   malehb_fafb14.tpsnm=nat::tpsreg(malehb_fafb14[3:5]*1e3, malehb_fafb14[6:8]*1e3)
 
   malecns_fafb14 <- malehb_fafb14
-  malecns_fafb14[3:5] <- halfbrain2wholebrain(malecns_fafb14[3:5], 'microns', warn=FALSE)
+  malecns_fafb14[3:5] <- halfbrain2wholebrain(malecns_fafb14[3:5], 'microns')
 
   malecns_fafb14.tps=nat::tpsreg(malecns_fafb14[3:5], malecns_fafb14[6:8])
   malecns_fafb14.tpsnm=nat::tpsreg(malecns_fafb14[3:5]*1e3, malecns_fafb14[6:8]*1e3)
@@ -22,7 +22,7 @@ halfbrain2wholebrain <- function(x, units=c("raw", "nm", "microns", "um"), warn=
   units=match.arg(units)
   xyz=xyzmatrix(x)
   raw_offset=4096
-  raw_xlim=c(6772, 35202)
+  raw_xlim=c(2676, 35202)
   offset=switch(units, raw=raw_offset, nm=raw_offset*8, raw_offset*8/1000)
   xlim=switch(units, raw=raw_xlim, nm=raw_xlim*8, raw_xlim*8/1000)
   nbad=sum(xyz[1,]<xlim[1] | xyz[1,]>=xlim[2], na.rm = T)
