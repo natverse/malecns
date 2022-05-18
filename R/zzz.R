8dfbed562ca66edc64f46e8240519f9509aa02a1
Gregory Jefferis <jefferis@gmail.com>
2022-05-18 21:36:10 +0100

    check malevnc load separately

--- R/zzz.R
+++ R/zzz.R
@@ -6,11 +6,17 @@
   op<-options()
   toset <- !(names(op.malecns) %in% names(op))
   if(any(toset)) options(op.malecns[toset])
-  res=try(requireNamespace('malevnc', quietly = TRUE) && choose_mcns())
-  if(inherits(res, 'try-error'))
-    message("Trouble choosing default malecns dataset.\nTry running dr_malecns() and then ",
-            "ask on #code or file an issue at\n",
-            "https://github.com/flyconnectome/malecns/issues")
+  if(!requireNamespace('malevnc', quietly = T))
+    message("Unable to load malevnc package.\nTry running dr_malecns() and then ",
+          "ask on #code or file an issue at\n",
+          "https://github.com/flyconnectome/malecns/issues")
+  else {
+    res=try(choose_mcns(), silent = F)
+    if(inherits(res, 'try-error'))
+      message("Trouble choosing default malecns dataset.\nTry running dr_malecns() and then ",
+              "ask on #code or file an issue at\n",
+              "https://github.com/flyconnectome/malecns/issues")
+  }
 
   res=try(mcns_register_xforms())
   if(inherits(res, 'try-error'))
