6a38a6c83a95dd77e8c089279471f0ee0569d46d
Gregory Jefferis <jefferis@gmail.com>
2022-05-18 21:19:13 +0100

    avoid outright error from package load
    * see https://github.com/flyconnectome/malecns/runs/6495138074?check_suite_focus=true#step:7:103

--- R/zzz.R
+++ R/zzz.R
@@ -6,15 +6,15 @@
   op<-options()
   toset <- !(names(op.malecns) %in% names(op))
   if(any(toset)) options(op.malecns[toset])
-  res=try(choose_mcns())
+  res=try(requireNamespace('malevnc', quietly = TRUE) && choose_mcns())
   if(inherits(res, 'try-error'))
-    warning("Trouble choosing default malecns dataset.\nTry running dr_malecns() and then ",
+    message("Trouble choosing default malecns dataset.\nTry running dr_malecns() and then ",
             "ask on #code or file an issue at\n",
             "https://github.com/flyconnectome/malecns/issues")
 
   res=try(mcns_register_xforms())
   if(inherits(res, 'try-error'))
-    warning("Trouble registering malencs xforms.\n",
+    message("Trouble registering malencs xforms.\n",
             "Ask on #code or file an issue at https://github.com/flyconnectome/malecns/issues")
   invisible()
 }
