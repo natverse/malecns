e3e796d0a4965b71332949809e2d82c7a36fab7f
Gregory Jefferis <jefferis@gmail.com>
2022-05-18 21:36:50 +0100

    Switch to packageStartupMessage

--- R/zzz.R
+++ R/zzz.R
@@ -7,20 +7,20 @@
   toset <- !(names(op.malecns) %in% names(op))
   if(any(toset)) options(op.malecns[toset])
   if(!requireNamespace('malevnc', quietly = T))
-    message("Unable to load malevnc package.\nTry running dr_malecns() and then ",
+    packageStartupMessage("Unable to load malevnc package.\nTry running dr_malecns() and then ",
           "ask on #code or file an issue at\n",
           "https://github.com/flyconnectome/malecns/issues")
   else {
     res=try(choose_mcns(), silent = F)
     if(inherits(res, 'try-error'))
-      message("Trouble choosing default malecns dataset.\nTry running dr_malecns() and then ",
+      packageStartupMessage("Trouble choosing default malecns dataset.\nTry running dr_malecns() and then ",
               "ask on #code or file an issue at\n",
               "https://github.com/flyconnectome/malecns/issues")
   }
 
   res=try(mcns_register_xforms())
   if(inherits(res, 'try-error'))
-    message("Trouble registering malencs xforms.\n",
+    packageStartupMessage("Trouble registering malencs xforms.\n",
             "Ask on #code or file an issue at https://github.com/flyconnectome/malecns/issues")
   invisible()
 }
