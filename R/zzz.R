9c16d693054db72e41c516890084b4c420a3c15c
Gregory Jefferis <jefferis@gmail.com>
2025-10-06 00:30:56 +0100

    don't use clio infrastructure to check public datasets

--- R/zzz.R
+++ R/zzz.R
@@ -6,22 +6,25 @@
   op<-options()
   toset <- !(names(op.malecns) %in% names(op))
   if(any(toset)) options(op.malecns[toset])
-  res=try(malevnc::choose_flyem_dataset(set=FALSE,
-                                 dataset = getOption('malecns.dataset')),
-          silent = F)
-  if (inherits(res, 'try-error'))
-    packageStartupMessage(
-      "Trouble choosing default malecns dataset.\nTry running dr_malecns() and then ",
-      "ask on #code or file an issue at\n",
-      "https://github.com/flyconnectome/malecns/issues"
-    )
-  ds=getOption('malecns.dataset', default = 'male-cns:v0.9')
+
+    ds=getOption('malecns.dataset', default = 'male-cns:v0.9')
+
   packageStartupMessage("Using malecns dataset `",ds,"`.")
   if(ds=='CNS') {
     packageStartupMessage(
     "You can switch to a snapshot dataset in this R session with:\n",
     'choose_mcns_dataset("male-cns:v0.9")'
     )
+    res=try(malevnc::choose_flyem_dataset(set=FALSE,
+                                          dataset = getOption('malecns.dataset')),
+            silent = F)
+    if (inherits(res, 'try-error'))
+      packageStartupMessage(
+        "Trouble choosing default malecns dataset.\nTry running dr_malecns() and then ",
+        "ask on #code or file an issue at\n",
+        "https://github.com/flyconnectome/malecns/issues"
+      )
+
   }
   packageStartupMessage(
     'See ?malecns section Package Options for details.')
